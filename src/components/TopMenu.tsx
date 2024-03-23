63b63d6cf4d8e915104aa536193571f37c56aacb
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-03-23 15:31:24 +0700

    change sign-in button

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { Link } from '@mui/material'
 import { useRouter } from 'next/navigation';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default async function TopMenu(){
     const session = await getServerSession(authOptions)
@@ -25,9 +26,9 @@ export default async function TopMenu(){
                     session? <Link href="/api/auth/signout">
                         <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
                         Sign-Out of {session.user?.name}</div></Link>
-                        : <Link href="/api/auth/signin">
-                        <div className='flex items-center h-full px-2 text-cyan-600 text-sm'>
-                        Sign-In</div></Link>
+                        : <Link href="/api/auth/signin">    
+                            <PersonIcon color="disabled">add_circle</PersonIcon>
+                        </Link>
                 }
             </div>
         </div>
