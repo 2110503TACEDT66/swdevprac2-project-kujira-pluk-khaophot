820f1759af32f7e241bf9c4bbaab43b8de3ad166
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-03-23 18:08:47 +0700

    styling top menu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -12,7 +12,7 @@ export default async function TopMenu(){
     return (
         <div className='h-[50px] bg-white fixed top-5 inset-x-0 z-30 flex flex-row mx-10'>
             <a href="/">
-                <Image src={'/img/logo.png'} className='h-[70%] w-auto absolute left-0'   width={0} height={0} sizes='100vh' alt='logo'/>
+                <Image src={'/img/logo.png'} className='h-[70%] w-auto absolute left-0 align-bottom top-[5px]'   width={0} height={0} sizes='100vh' alt='logo'/>
             </a>
 
             <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
@@ -24,11 +24,11 @@ export default async function TopMenu(){
             <div>
                 {
                     session? <Link href="/api/auth/signout">
-                        <div className='flex items-center absolute right-0 h-full px-2 text-cyan-600 text-sm'>
+                        <div className='flex items-center absolute right-0 h-full px-2 text-cyan-600 text-sm justitfy-center'>
                         Sign-Out of {session.user?.name}</div></Link>
                         : 
                         
-                        <Link href="/api/auth/signin" className='absolute right-0'>    
+                        <Link href="/api/auth/signin" className='absolute right-0 top-[5px]'>    
                             <PersonIcon color="disabled">add_circle</PersonIcon>
                         </Link>
                 }
--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -2,7 +2,8 @@ import Link from 'next/link'
 
 export default function TopMenuItem ({title, pageRef}: {title:string, pageRef:string}){
     return(
-        <Link className='w-[120px] text-center mt-auto mb-auto font-mono text-sm text-grey-500 mx-[20px] text-xl' href={pageRef}>
+        <Link className='w-[120px] text-center mt-auto mb-auto font-mono text-sm text-grey-500 mx-[20px] text-xl justify-center
+        hover:text-slate-500' href={pageRef}>
             {title}
         </Link>
     )
