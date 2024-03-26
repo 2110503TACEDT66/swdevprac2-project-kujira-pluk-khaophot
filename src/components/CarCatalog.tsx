c9458d3871f9910e8da0c2aba6f2fd960fdcb485
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-03-26 21:34:23 +0700

    add key to CarCatalog

--- src/components/CarCatalog.tsx
+++ src/components/CarCatalog.tsx
@@ -8,7 +8,7 @@ export default async function CarCatalog({carJson}:{carJson:CarJson}) {
         <div className="flex flex-col mt-24">
             {
                 carJsonReady.data.map((carItem:CarItem)=>(
-                    <ProductCard carName={carItem.car} ownername={carItem.name} 
+                    <ProductCard carName={carItem.car} ownername={carItem.name} key={carItem._id}
                     ownerAddress={carItem.address} carId={carItem._id} imgSrc={carItem.picture}/>
                     )
                 )
